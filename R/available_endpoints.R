73d4aed5d17009ced769bc01dafff78209ef8cc3
Justin Taylor <justin.taylor.data.science@gmail.com>
2022-08-18 12:37:46 +0100

    Adding a file 'available_endpoints.R' where we will define the S3 constructors that correspond to the Benchling API endpoints as methods are written for them

---
+++ R/available_endpoints.R
@@ -0,0 +1,10 @@
+# available_endpoints.R
+# These are the available warehouse/API endpoints. The names of these endpoints
+# correspond directly to the API endpoints listed in the Benchling API 
+# documentation (https://benchling.com/api/). New endpoints will be added
+# to this file as S3 methods are written for them. 
+
+entry <- function() {
+  structure("Entry", class="Entry")
+}
+
