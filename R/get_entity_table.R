dc83a1972a504aaa29fcc196aa07394b09e118ea
Justin Taylor <justin.taylor.data.science@gmail.com>
2022-08-22 14:03:26 +0100

    Added get_entity_table and list_entity_columns functions for working with entity fields in data warehouse tables. These functions were migrated from the original HS Benchling package with little to no modifications, except for the function names themselves.

---
+++ R/get_entity_table.R
@@ -0,0 +1,62 @@
+# get_entity_table.R
+
+#' Get additional information about entities in a warehouse table.
+#' 
+#' Given a data frame retrieved from the Benchling warehouse, extract the
+#' additional tables in the warehouse that correspond to the entities in the 
+#' data frame. 
+#' 
+#' @include vec2sql_tuple.R
+#' @param conn Database connection opened by `warehouse_connect`
+#' @param df Data frame with one or more entity columns. The data frame
+#' must also have a column called `schema`, which indicates the schema
+#' name of the warehouse table. 
+#' @param column Name of the entity column. 
+#' @param return_cols Character vector of warehouse column names to include
+#'  in results. If `return_cols` is missing, then every column will be returned. 
+#' @return ddata frame with rows from warehouse table that correspond to 
+#' entities found in the input data frame (`df`). 
+#' @export
+get_entity_table <- function(conn, df, column, return_cols=c('id', 'name$')) {
+  # Raise exception if the specified column isn't in the data frame. 
+  assertthat::assert_that(
+    column %in% colnames(df),
+    msg = glue::glue("{`column`} is not a column in the data frame!"))
+  # If the field is multi-select, then extract all identifiers
+  if (class(df[[column]]) == 'pq_jsonb') {
+    id_list <- .vec2sql_tuple(
+      Filter(function(x) (x != list()),
+             purrr::map(as.character(df[[column]]),
+                        ~ RJSONIO::fromJSON(.))) %>%
+        unlist)
+    # The code above will return an empty list as a string if 
+    # the entity column is completely empty.
+    if (id_list == '()') {
+      return(NA)
+    }
+  } else { # Get all identifiers from single value columns
+    id_list <- .vec2sql_tuple(
+      Filter(function(x) (!is.na(x)), unique(df[[column]])))
+  }
+  # If user doesn't specify any columns to return,
+  # then retrieve all of them. 
+  if (missing(return_cols)) {
+    return_cols <- '*'
+  }
+  if (length(return_cols) == 0) {
+    return_cols <- '*'
+  }
+  
+  # Pull these columns from the warehouse
+  warehouse_cols <- paste0(return_cols, collapse=',')
+  
+  # In some results schemas, the warehouse name of a column will be different than
+  # the warehouse name of the corresponding entity table, so one must construct a 
+  # mapping from the former to the latter by traversing some of the schema tables. 
+  column_map <- .map_entity_field_names_to_warehouse_tables(conn, df)
+  query <- glue::glue("SELECT {`warehouse_cols`} FROM {column_map[column]}
+                        WHERE id IN {`id_list`}")
+  mapping <- DBI::dbGetQuery(
+    conn, query)
+  return(mapping)
+}
---
+++ R/list_entity_columns.R
@@ -0,0 +1,31 @@
+# list_entity_columns.R
+
+#' List the names and indices of entity columns in a data frame corresponding
+#' to a warehouse table. 
+#' 
+#' @importFrom magrittr %>%
+#' @param conn Database connection opened with `warehouse_connect`
+#' @param df Data frame with entity columns
+#' @return Vector where the names are the names of entity columns in the data frame
+#' and the values are the corresponding indices. 
+#' @examples 
+#' conn <- warehouse_connect('hemoshear')
+#' res <- DBI::dbGetQuery(conn, "SELECT * FROM plate")
+#' list_entity_columns(conn, res)
+#' DBI::dbDisconnect(conn)
+#' @export 
+list_entity_columns <- function(conn, df) {
+  if (!('schema' %in% colnames(df))) {
+    stop("'schema' column is missing from the input data.frame.
+         Verify that the data.frame is a valid warehouse table.")
+  }
+  schema_id <- DBI::dbGetQuery(
+    conn, glue::glue("SELECT id FROM schema WHERE system_name = {shQuote(unique(df$schema))}")) %>%
+    as.character()
+  res <- DBI::dbGetQuery(conn, glue::glue(
+    "SELECT * FROM schema_field WHERE schema_id = {shQuote(schema_id)}")) %>%
+    dplyr::filter(!is.na(target_schema_id))
+  entities <- which(colnames(df) %in% res$system_name)
+  names(entities) <- colnames(df)[entities]
+  entities
+}
