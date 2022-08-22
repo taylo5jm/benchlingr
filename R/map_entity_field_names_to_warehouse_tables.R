d6ecd43fdb783cd4e2db213cd1a1dc736f5333e9
Justin Taylor <justin.taylor.data.science@gmail.com>
2022-08-22 12:47:23 +0100

    Adding function to map entity field names in one schema to corresponding warehouse table names. Also added the vec2sql_tuple function to assist in creating tuples for use in SQL queries. As part of this commit, the DESCRIPTION and NAMESPACE files were updated, since the first function mentioned uses the second function mentioned.

---
+++ R/map_entity_field_names_to_warehouse_tables.R
@@ -0,0 +1,43 @@
+#' Map entity field names in one warehouse table to corresponding warehouse
+#' table with the relevant entities. 
+#' 
+#' 
+#' A schema field name can differ from the schema name itself, so this function
+#' uses the `schema` and `schema_field` tables in the warehouse to link 
+#' the field name in one schema to the actual name of the entity.
+#' 
+#' @include vec2sql_tuple.R
+#' @importFrom magrittr %<>% %>%
+#' @param conn Database connection opened by `warehouse_connect`
+#' @param df data.frame with one or more fields that are entity types. 
+#' @return A vector where the names are the warehouse names for the fields 
+#' in the schema and the values are the warehouse names for the entities in the 
+#' schema 
+.map_entity_field_names_to_warehouse_tables <- function(conn, df) {
+  if (!('schema' %in% colnames(df))) {
+    stop("'schema' column is missing from the input data.frame.
+         Verify that the data.frame is a valid warehouse table.")
+  }
+  # Get the id of the schema
+  schema_id <- DBI::dbGetQuery(conn, 
+    glue::glue("SELECT id FROM schema WHERE system_name = {shQuote(unique(df$schema))}")) %>%
+    as.character()
+  # Get all fields for the schema
+  res <- DBI::dbGetQuery(conn, glue::glue(
+    "SELECT * FROM schema_field WHERE schema_id = {shQuote(schema_id)}"))
+  # Retain only the entity fields in the schema
+  res %<>% dplyr::filter(!is.na(target_schema_id))
+  # Get the actual names of the entity types in the target schema
+  schema <- DBI::dbGetQuery(conn, 
+    glue::glue("SELECT * FROM schema WHERE id IN {.vec2sql_tuple(res$target_schema_id)}"))
+  # Trim the data frame down to the name and ID we care about. 
+  ## The first column here is the name of the schema
+  ## The second column is the name of the *field* that is the entity type. 
+  schema <- dplyr::select(schema, id, system_name)
+  colnames(schema) <- c('target_schema_id', 'entity_system_name')
+  # Join the names we need to the original schema
+  res %<>% dplyr::inner_join(schema, by='target_schema_id')
+  mapping <- res$entity_system_name
+  names(mapping) <- res$system_name
+  mapping
+}
---
+++ R/vec2sql_tuple.R
@@ -0,0 +1,19 @@
+# vec2sql_tuple.R
+
+#' Convert a vector into a valid tuple that can be used
+#' in SQL queries. 
+#'
+#' @importFrom magrittr %>%
+#' @param x Character or numeric vector
+#' @return A character vector that represents a SQL tuple to be
+#' used in SQL queries.
+#' @examples
+#' x <- c('A', 'B', 'C')
+#' res <- .vec2sql_tuple(x)
+#' y <- c(1, 2)
+#' res <- .vec2sql_tuple(y)
+.vec2sql_tuple <- function(x) {
+  shQuote(x) %>%
+    paste0(., collapse=',') %>%
+    paste0('(', ., ')')
+}
