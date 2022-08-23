36cb64299c7e9ad005803bb24fd82ead89fe3801
Justin Taylor <justin.taylor.data.science@gmail.com>
2022-08-23 18:04:05 +0100

    Added the inst/download_files.py helper script for invoking the Python SDK to download file attachments.

---
+++ inst/download_files.py
@@ -0,0 +1,23 @@
+# download_file.py
+## Download file attachments from Benchling
+## ===================================
+import json
+import os
+from pathlib import Path
+from typing import Dict
+
+
+def download_files(client, file_map: Dict[str, str]): -> None
+    """
+      Download file attachments from Benchling
+      
+      :param client: Benchling API client object. See `benchling_api_client.py`
+      :param file_map: Dictionary where keys are the blob identifiers and 
+        values are the corresponding file paths to be created on the local machine.
+    """
+    for blob_id,destination_path in file_map.items():
+        # Get the notebook entry
+        try:
+            client.blobs.download_file(blob_id=blob_id, destination_path=Path(destination_path))
+        except FileExistsError:
+            pass
