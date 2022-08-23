773764e92930ed49f5ce73163a22aa9ebdde67b3
Justin Taylor <justin.taylor.data.science@gmail.com>
2022-08-23 14:49:10 +0100

    Didn't add the file in my last commit.

---
+++ R/util.R
@@ -0,0 +1,15 @@
+# util.R
+
+#' Raise exception if the `schema` column is not in the data frame. 
+#' 
+#' The `schema` column appears in every data warehouse table. This function
+#' stops function execution if `schema` is not in the data frame. 
+#' @param df data.frame with table from the data warehouse. 
+#' @return Used for side effect only.
+#' 
+is_schema_in_dataframe <- function(df) {
+  if (!('schema' %in% colnames(df))) {
+    stop("'schema' column is missing from the input data.frame.
+           Verify that the data.frame is a valid warehouse table.")
+  }
+}
