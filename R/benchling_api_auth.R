dcbebc2f911b0fa86b6fab9593cebd218d7fff9e
Justin Taylor <justin.taylor.data.science@gmail.com>
2022-08-30 15:08:26 -0400

    Cleaning up the benchling_api_auth comments

--- R/benchling_api_auth.R
+++ R/benchling_api_auth.R
@@ -20,18 +20,8 @@ benchling_api_auth <- function(
          in R using the following command:
          usethis::edit_r_environ()")
   }
-  #if (env_type == 'virtualenv') {
-  #  
-  #  reticulate::use_virtualenv(virtualenv=env_name)
-  #} else {
-  #  # Sys.setenv(RETICULATE_PYTHON=env_name)
-  #  reticulate::use_condaenv(condaenv = env_name)
-  #}
   reticulate::source_python(
     system.file("python", "benchling_api_client.py", package = "benchlingr"))
   return(benchling_api_client(tenant, api_key))
 }
-#Sys.setenv(RETICULATE_PYTHON='/Users/justintaylor/.virtualenvs/benchling-reticulate/bin/python')
-#client <- benchling_api_auth(tenant="https://hemoshear.benchling.com",
-#                             env_name="benchling-reticulate", env_type="virtualenv")
-#client$entries$get_entry_by_id('etr_FbZuzgv6')
+
