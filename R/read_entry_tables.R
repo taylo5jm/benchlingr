85f19b653695ec717662ea75ce58369f9e6d9dad
Justin Taylor <justin.taylor.data.science@gmail.com>
2022-08-30 17:00:34 -0400

    Updating read_entry_tables to work with the JSON payload when invoking the Python SDK in R, instead of saving the JSON payload to a file as we have been doing in the private HS package.

--- R/read_entry_tables.R
+++ R/read_entry_tables.R
@@ -17,14 +17,49 @@
 #' # entry.
 #' a_table <- read_table(entry, day=1, table_index=table_indices[[1]][1])
 #' }
+#' 
+
 read_entry_table <- function(entry, day, table_index) {
   a_table <- entry$days[[day]]$notes[[table_index]]
-  a_table$table$rows %>%
-    purrr::map(~ .[[1]]) %>%
+
+  direct_from_api <- FALSE
+  if ((class(a_table)[1] == "benchling_api_client.v2.stable.models.table_note_part.TableNotePart")) {
+    if (is.character(a_table$table$column_labels)) {
+      columns <- snakecase::to_snake_case(a_table$table$column_labels)
+    } else { # in this case, the column_labels slot is a list of NULLs.
+      columns <- NA
+    }
+    direct_from_api <- TRUE
+  } else {
+    columns <- tryCatch({
+      snakecase::to_snake_case(a_table[[1]][[1]])},
+      error = function(e) {NA})
+  }
+  
+  .extract_rows <- function(rows, from_api) {
+    .extract_row <- function(row) {
+      if (class(row[[1]])[1] == "benchling_api_client.v2.stable.models.entry_table_row.EntryTableRow") {
+        purrr::map(row, ~ lapply(.$cells, function(x) (x['text'])) %>%
+                     unlist)
+      } else {
+        purrr::map(row, ~ .)
+      }
+    }
+    if (from_api) {
+      .extract_row(rows)
+    } else {
+      purrr::map(rows, ~ .[[1]] %>% unlist)
+    }
+  }
+  res <- a_table$table$rows %>%
+    .extract_rows(., direct_from_api) %>%
     do.call('rbind', .) %>% 
     as.data.frame()
+  if (!all(is.na(columns))) {
+    colnames(res) <- columns
+  } 
+  res
 }
-
 
 
 #' Read all unstructured tables in a notebook entry. 
@@ -39,6 +74,8 @@ read_entry_table <- function(entry, day, table_index) {
 #' @return List of data frames representing the unstructured tables in
 #' the notebook entry.
 #' @examples \dontrun{
+#' client <- benchling_api_auth(tenant="https://hemoshear.benchling.com")
+#' client$entries$get_entry_by_id
 #' entry <- get_entry(id = "etr")
 #' tables <- read_tables(entry)
 #' }
