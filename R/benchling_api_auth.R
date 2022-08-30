452148270e4a5512a143a018ad9a26be65a54711
Justin Taylor <justin.taylor.data.science@gmail.com>
2022-08-30 15:03:21 -0400

    Pushing the changes to benchling_api_auth which allow user to bypass the environment names and types

--- R/benchling_api_auth.R
+++ R/benchling_api_auth.R
@@ -13,17 +13,25 @@
 #' }
 
 benchling_api_auth <- function(
-  tenant, api_key=Sys.getenv("BENCHLING_API_KEY"),
-  condaenv="benchling-reticulate") {
+  tenant, api_key=Sys.getenv("BENCHLING_API_KEY")) {
   if (api_key == "") {
     stop("'api_key' cannot be an empty string. Set the 'BENCHLING_API_KEY' 
          environment variable in your .Renviron file. Edit the .Renviron file
          in R using the following command:
          usethis::edit_r_environ()")
   }
-  reticulate::use_condaenv(condaenv = condaenv)
+  #if (env_type == 'virtualenv') {
+  #  
+  #  reticulate::use_virtualenv(virtualenv=env_name)
+  #} else {
+  #  # Sys.setenv(RETICULATE_PYTHON=env_name)
+  #  reticulate::use_condaenv(condaenv = env_name)
+  #}
   reticulate::source_python(
     system.file("python", "benchling_api_client.py", package = "benchlingr"))
   return(benchling_api_client(tenant, api_key))
 }
-
+#Sys.setenv(RETICULATE_PYTHON='/Users/justintaylor/.virtualenvs/benchling-reticulate/bin/python')
+#client <- benchling_api_auth(tenant="https://hemoshear.benchling.com",
+#                             env_name="benchling-reticulate", env_type="virtualenv")
+#client$entries$get_entry_by_id('etr_FbZuzgv6')
