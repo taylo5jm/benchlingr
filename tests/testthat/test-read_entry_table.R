2de94a24a7f5f7b1aa824707a3e51c25a22151c2
Justin Taylor <justin.taylor.data.science@gmail.com>
2022-09-16 16:11:28 -0400

    Adding read_entry_table unit-test

---
+++ tests/testthat/test-read_entry_table.R
@@ -0,0 +1,10 @@
+library(benchlingr)
+client <- benchling_api_auth(tenant="https://hemoshear-dev.benchling.com",
+                             api_key=Sys.getenv("BENCHLING_DEV_API_KEY"))
+entry <- client$entries$get_entry_by_id("etr_T3WZTyAe")
+a_table <- read_entry_table(entry, day = 1, table_index = 2)
+
+
+test_that("read_entry_table works", {
+  expect_equal(nrow(a_table), 6)
+})
