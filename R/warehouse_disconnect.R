1649609ee1d9dfda9dbb78873ea55693172d16a4
Aakshay Kumar <ak5wm@virginia.edu>
2023-02-10 13:31:21 -0500

    Modifying warehouse_connect.R and warehouse_disconnect.R and their test cases to account for when the user does not provide a legitimate tenant, username or password in generating a warehouse connection object and for when the connection the user wants to disconnect from is not valid, a database connection object or even PqConnection-class

--- R/warehouse_disconnect.R
+++ R/warehouse_disconnect.R
@@ -9,21 +9,26 @@
 #' @return TRUE, invisibly
 #' @examples 
 #' \dontrun{
-#' conn <- warehouse_connect("hemoshear-dev", 
-#'     username = Sys.getenv("BENCHLING_DEV_WAREHOUSE_USERNAME"),
-#'     password = Sys.getenv("BENCHLING_DEV_WAREHOUSE_PASSWORD"))
+#' conn <- warehouse_connect(tenant = "hemoshear-dev", 
+#'     username = Sys.getenv("BENCHLING_WAREHOUSE_USERNAME"),
+#'     password = Sys.getenv("BENCHLING_WAREHOUSE_PASSWORD"))
 #' warehouse_disconnect(conn)    
 #' }
 
-warehouse_disconnect <- function(conn){
-  if (DBI::dbIsValid(conn) == TRUE) {
-    if (class(conn) == "PqConnection") {
-      DBI::dbDisconnect(conn) # returns true invisibly
-    } else {
-      stop("Input was not a PqConnection class object.")
+warehouse_disconnect <- function(conn) {
+  if (inherits(conn,"DBIConnection") == FALSE) { # Checks to see if the input is not a DBIConnection object
+    stop("Input is not a database connection object.")
+  } else if (inherits(conn,"DBIConnection") == TRUE) {
+    if (class(conn) != "PqConnection") { # Checks to see if the input's class is PqConnection
+      stop("Input is not a PqConnection class object.")
+    } else if (class(conn) == "PqConnection") {
+      if (DBI::dbIsValid(conn) == FALSE) { # Checks to see if the input is still valid
+        stop("The input is no longer valid or has already been disconnected.")
+      } else if (DBI::dbIsValid(conn) == TRUE) {
+          DBI::dbDisconnect(conn) # returns true invisibly
+      }
     }
-  } else {
-    stop("The input is no longer valid.")
   }
 }
+
 
