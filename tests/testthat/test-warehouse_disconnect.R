8f252156d6864df05076d4e4503fbaad30424ecf
Aakshay Kumar <ak5wm@virginia.edu>
2023-02-10 18:43:26 -0500

    Adding text to test-warehouse_disconnect.R to better explain test cases

--- tests/testthat/test-warehouse_disconnect.R
+++ tests/testthat/test-warehouse_disconnect.R
@@ -1,6 +1,6 @@
 # test-warehouse_disconnect.R
 
-# test case 1
+# test case 1 - verifying that a disconnect was successful
 conn1 <- warehouse_connect(tenant = "hemoshear-dev", 
                            username = Sys.getenv("BENCHLING_WAREHOUSE_USERNAME"),
                            password = Sys.getenv("BENCHLING_WAREHOUSE_PASSWORD"))
@@ -9,7 +9,8 @@ test_that("Verify that a disconnect was successful when the user
             expect_true(warehouse_disconnect(conn1))
 })
 
-# test case 2
+# test case 2 - checking if the function errors out when provided with an input 
+# that is neither a database connection object nor PqConnection-class
 conn2 <- "object"
 test_that("Check if the function errors out with an informative message
           when the user provides an input that is not a database connection 
@@ -18,7 +19,8 @@ test_that("Check if the function errors out with an informative message
                          "Input is not a database connection object.")
           })
 
-# test case 3
+# test case 3 - checking if the function errors out when provided with an input
+# that is PqConnection-class but not a database connection object
 conn3 <- "object"
 class(conn3) <- "PqConnection"
 test_that("Check if the function errors out with an informative message
@@ -28,7 +30,8 @@ test_that("Check if the function errors out with an informative message
                          "Input is not a database connection object.")
           })
 
-# test case 4
+# test case 4 - checking if the function errors out when provided with an input
+# that is a database connection object but it is not PqConnection-class
 conn4 <- DBI::dbConnect(RSQLite::SQLite(), "memory")
 test_that("Check if the function errors out with an informative message
           when the user provides an input that is a database connection 
@@ -38,7 +41,10 @@ test_that("Check if the function errors out with an informative message
           })
 DBI::dbDisconnect(conn4)
 
-# test case 5
+# test case 5 - checking if the function errors with an informative message 
+# when the user provides an input that is a database connection object and 
+# PqConnection-class but it is no longer valid or has already been 
+# disconnected
 conn5 <- warehouse_connect(tenant = "hemoshear-dev",
                            username = Sys.getenv("BENCHLING_WAREHOUSE_USERNAME"),
                            password = Sys.getenv("BENCHLING_WAREHOUSE_PASSWORD"))
