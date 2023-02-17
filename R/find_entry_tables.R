ee0d638d0b821d725f693d1e03b4ae05d918bebc
Aakshay Kumar <ak5wm@virginia.edu>
2023-02-17 00:51:58 -0500

    Modifying find_entry_tables.R to complete Issue 44

--- R/find_entry_tables.R
+++ R/find_entry_tables.R
@@ -7,7 +7,7 @@
 #' located in a notebook entry, as information in those tables must be retrieved
 #' from other API endpoints / data warehouse tables. 
 #' 
-#' @param json JSON response from GET /entry/{entry_id}. *** Link the appropriate
+#' @param entry entry response from GET /entry/{entry_id}. *** Link the appropriate
 #' Benchling API endpoint here. 
 #' @param min_rows Keep tables with this number of rows or more.
 #' @return List where names are days and elements are indices in the `notes` attribute that
@@ -16,21 +16,21 @@
 #' client <- benchlingr::benchling_api_auth(
 #'     tenant="https://hemoshear-dev.benchling.com",
 #'     api_key = Sys.getenv("BENCHLING_DEV_API_KEY"))
-#' json <- client$entries$get_entry_by_id("etr_T3WZTyAe")
-#' benchlingr::find_entry_tables(json)
+#' entry <- client$entries$get_entry_by_id("etr_T3WZTyAe")
+#' benchlingr::find_entry_tables(entry)
 #' }
 #' @export
 
-find_entry_tables <- function(json, min_rows=NULL) {
-  .find_tables <- function(json, min_rows=NULL) {
-    tables <- which(purrr::map_lgl(json$notes, ~ 'table' %in% names(.)))
-    table_lengths <- purrr::map_dbl(json$notes[tables], 
+find_entry_tables <- function(entry, min_rows=NULL) {
+  .find_tables <- function(entry, min_rows=NULL) {
+    tables <- which(purrr::map_lgl(entry$notes, ~ 'table' %in% names(.)))
+    table_lengths <- purrr::map_dbl(entry$notes[tables], 
                                     ~ length(.$table$rows))
     if (!is.null(min_rows)) {
       tables <- tables[which(table_lengths >= min_rows)]
     }
     return(tables)
   }
-  res <- purrr::map(json$days, ~ .find_tables(., min_rows=min_rows))
+  res <- purrr::map(entry$days, ~ .find_tables(., min_rows=min_rows))
   res
 }
--- tests/testthat/test-read_entry_table.R
+++ tests/testthat/test-read_entry_table.R
@@ -7,16 +7,16 @@ client <- benchling_api_auth(tenant="https://hemoshear-dev.benchling.com",
 entry <- client$entries$get_entry_by_id("etr_T3WZTyAe")
 
 test_that("read_entry_table works", {
-  expect_equal(nrow(read_entry_table(entry, day = 1, table_position = 2, table_name = TRUE)), 6)
-  expect_equal(ncol(read_entry_table(entry, day = 1, table_position = 2, table_name = TRUE)), 3)
-  expect_equal(ncol(read_entry_table(entry, day = 1, table_position = 2, table_name = FALSE)), 2)
-  expect_identical(read_entry_table(entry, day = 1, table_position = 2, table_name = TRUE),
+  expect_equal(nrow(read_entry_table(entry, day = 1, table_position = 2, return_table_name = TRUE)), 6)
+  expect_equal(ncol(read_entry_table(entry, day = 1, table_position = 2, return_table_name = TRUE)), 3)
+  expect_equal(ncol(read_entry_table(entry, day = 1, table_position = 2, return_table_name = FALSE)), 2)
+  expect_equal(read_entry_table(entry, day = 1, table_position = 2, return_table_name = TRUE),
                    data.frame(sample_id = c("1", "2", "3", "4", "5", "6"), 
                               treatment = c("Drug A", "Drug A", "Drug A", "Control", "Control","Control"),
-                              table_name = c("Experimental Conditions", "Experimental Conditions", 
+                              return_table_name = c("Experimental Conditions", "Experimental Conditions", 
                                              "Experimental Conditions", "Experimental Conditions", 
                                              "Experimental Conditions", "Experimental Conditions")))
-  expect_identical(read_entry_table(entry, day = 1, table_position = 2, table_name = FALSE),
+  expect_equal(read_entry_table(entry, day = 1, table_position = 2, return_table_name = FALSE),
                    data.frame(sample_id = c("1", "2", "3", "4", "5", "6"), 
                               treatment = c("Drug A", "Drug A", "Drug A", "Control", "Control","Control")))
 })
