c0e1fcc9b33c02f8efea4e3b4a1916023326f2ce
Aakshay Kumar <ak5wm@virginia.edu>
2023-02-16 19:28:30 -0500

    Modifying test-read_entry_table.R

--- tests/testthat/test-read_entry_table.R
+++ tests/testthat/test-read_entry_table.R
@@ -1,5 +1,7 @@
 # test-read_entry_table.R 
 
+library(magrittr)
+
 client <- benchlingr::benchling_api_auth(tenant="https://hemoshear-dev.benchling.com",
                                          api_key = Sys.getenv("BENCHLING_DEV_API_KEY"))
 entry <- client$entries$get_entry_by_id("etr_IWLMFYhR")
