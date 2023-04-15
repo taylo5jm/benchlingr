233c960fa7d196d18bb58366f35e470c41ecf050
Aakshay Kumar <ak5wm@virginia.edu>
2023-04-15 01:34:13 -0400

    Making further modifications to test cases for .list_api_contents.R

--- tests/testthat/test-.list_api_contents.R
+++ tests/testthat/test-.list_api_contents.R
@@ -10,14 +10,14 @@ test_that("Check that .list_api_contents.R generates an error when given an inva
 
 # test case 2
 test_that("Check that .list_api_contents.R generates an appropriate list of entities when given a valid input for contents", {
-  expect_type(.list_api_contents(contents = "all"),
+  expect_equal(class(.list_api_contents(contents = "all")),
               "list")
-  expect_type(.list_api_contents(contents = "entity schema"),
+  expect_equal(class(.list_api_contents(contents = "entity schema")),
               "list")
-  expect_type(.list_api_contents(contents = "list contents"),
+  expect_equal(class(.list_api_contents(contents = "list contents")),
               "list")
-  expect_type(.list_api_contents(contents = "single-get endpoint"),
+  expect_equal(class(.list_api_contents(contents = "single-get endpoint")),
               "list")
-  expect_type(.list_api_contents(contents = "bulk-get endpoints"),
+  expect_equal(class(.list_api_contents(contents = "bulk-get endpoints")),
               "list")
 })
